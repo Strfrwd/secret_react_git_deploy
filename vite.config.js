3e7b36de96574e71c456339ca2dc796c8a0cb6c0
strfrwd777 <strfrwd777@gmail.com>
2025-06-11 20:24:07 -0300

    Atualizações no vite.config para deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   //plugins: [react()],
-    base: "/",
+    base: "/secret_react_git_deploy",
     plugins: [react()],
     preview: {
      port: 3000,
